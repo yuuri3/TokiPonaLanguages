49446f3854c569ea176b2cad69847860d6f7c09e
fukurounagi <unagifukuro@gmail.com>
2026-01-24 00:30:43 +0900

    fix 実行ファイルと出力ファイルを無視

--- compile.bat
+++ compile.bat
@@ -2,8 +2,8 @@
 setlocal
 
 pushd "%~dp0"
-g++ -o a header/Utility.cpp header/Random.cpp header/Language.cpp main.cpp -std=c++2a
-start a.exe
+g++ -o ignore/a header/Utility.cpp header/Random.cpp header/Language.cpp main.cpp -std=c++2a
+start ignore/a.exe
 popd
 
 pause
