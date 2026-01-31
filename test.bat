90c43329d700f4838d58b1e46329ea6fdb61a0de
fukurounagi <unagifukuro@gmail.com>
2026-01-31 19:01:13 +0900

    feat 時間計測テスト

--- test.bat
+++ test.bat
@@ -6,7 +6,11 @@ pushd "%~dp0"
 del /q "ignore\test_data\*"
 
 g++ -o ignore/a Utility.cpp Random.cpp Language.cpp test.cpp -std=c++2a
+
+call time.bat START
 start /wait "" ignore/a.exe
+call time.bat STOP
+call time.bat PRINT
 
 dir "ignore\test_data"
 
