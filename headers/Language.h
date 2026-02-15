16d68f6b502f25a74e95a0e293b5d218cec7cae1
fukurounagi <unagifukuro@gmail.com>
2026-02-15 11:27:44 +0900

    add Phoneme.h

--- headers/Language.h
+++ headers/Language.h
@@ -1,37 +1,8 @@
 #include "Utility.h"
 #include "Random.h"
+#include "Phoneme.h"
 #include <vector>
 #include <string>
-#include <map>
-
-/**
- * @brief 音韻
- *
- */
-struct Phomene
-{
-    // 調音方法
-    int Manner;
-    // 調音部位
-    int Place;
-
-    bool operator==(const Phomene &other) const
-    {
-        return (Manner == other.Manner && Place == other.Place);
-    }
-
-    bool operator!=(const Phomene &other) const
-    {
-        return !(*this == other);
-    }
-
-    bool operator<(const Phomene &other) const
-    {
-        if (Manner != other.Manner)
-            return Manner < other.Manner;
-        return Place < other.Place;
-    }
-};
 
 /**
  * @brief 意味ベクトル
