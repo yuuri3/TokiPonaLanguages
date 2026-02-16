c8aabd108dc9f80f33d65bfdbf26ef78a89564f0
fukurounagi <unagifukuro@gmail.com>
2026-02-16 11:13:13 +0900

    refactor 45 # コンパイル用 .bat の pause を削除

--- TokiPonaLanguages.bat
+++ TokiPonaLanguages.bat
@@ -4,5 +4,3 @@ setlocal
 pushd "%~dp0"
 g++ -o ignore/a headers/Utility.cpp headers/Random.cpp headers/Meaning.cpp headers/Word.cpp headers/WordForSimulation.cpp headers/LanguageDifference.cpp headers/PhonemeConverter.cpp headers/LanguageFamily.cpp headers/LanguageFamilySimulator.cpp TokiPonaLanguages.cpp -std=c++2a -lcomdlg32
 popd
-
-pause
