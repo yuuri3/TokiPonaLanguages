5b5b0227bda52ffd619b891c5e25d1bf72c88463
fukurounagi <unagifukuro@gmail.com>
2026-02-20 06:30:21 +0900

    feat 66 メインウィンドウの表示

---
+++ include/MainWindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#pragma once
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QVBoxLayout>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    virtual ~MainWindow();
+
+private slots:
+    void onRunButtonClicked();
+
+private:
+    QPushButton *simulateButton;
+    QPushButton *newButton;
+    QPushButton *openFileButton;
+};
+
+#endif
