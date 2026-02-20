1f206b7ffd9f78feb61bfbf6750f0802131b23ee
fukurounagi <unagifukuro@gmail.com>
2026-02-21 02:57:41 +0900

    feat 8 Word のデータ構造を OTM-JSON 対応

--- include/Word.h
+++ include/Word.h
@@ -2,6 +2,8 @@
 
 #include "Phoneme.h"
 #include <vector>
+#include <map>
+#include <string>
 
 class Language;
 
@@ -11,8 +13,21 @@ class Language;
  */
 struct Word
 {
-    // 発音
+    // entry
+    //   ID
+    int ID;
+    //   form
     std::vector<Phomene> Form;
+    // translation <title,form>
+    std::map<std::string, std::vector<std::string>> Translations;
+    // tags
+    std::vector<std::string> Tags;
+    // contents <title,text>
+    std::map<std::string, std::string> Contents;
+    // variations <title,form>
+    std::map<std::string, std::vector<Phomene>> Variations;
+    // relations <title,entry>
+    std::map<std::string, int> Relations;
 
     bool operator==(const Word &other) const
     {
