58debb1fdeb3d068ec8e8680d0253a2a816b1ae6
fukurounagi <unagifukuro@gmail.com>
2026-03-01 09:44:29 +0900

    add EditGeometryDialog::ShowContextMenu
    表右クリック時イベント
    114 地域編集ダイアログ > 編集

--- src/EditGeometryDialog.cpp
+++ src/EditGeometryDialog.cpp
@@ -9,6 +9,10 @@ EditGeometryDialog::EditGeometryDialog(QWidget *parent)
 
     MainTable_ = new QTableWidget(this);
     layout->addWidget(MainTable_);
+
+    MainTable_->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(MainTable_, &QTableWidget::customContextMenuRequested,
+            this, &EditGeometryDialog::ShowContextMenu);
 }
 
 /**
@@ -63,5 +67,56 @@ void EditGeometryDialog::UpdateTable()
     if (Languages_ && Place_ && Period_)
     {
         DisplayTable(MainTable_, Languages_->GetGeography(), true);
+    }
+}
+
+/**
+ * @brief 表右クリック時イベント
+ *
+ */
+void EditGeometryDialog::ShowContextMenu(const QPoint &pos)
+{
+    // クリックされた位置のアイテムを取得
+    QModelIndex index = MainTable_->indexAt(pos);
+    if (!index.isValid())
+        return; // セルのない場所なら何もしない
+
+    QMenu menu(this);
+    QAction *addUpRow = menu.addAction("上に行追加");
+    QAction *addDownRow = menu.addAction("下に行追加");
+    QAction *deleteRow = menu.addAction("行削除");
+    QAction *addRightColumn = menu.addAction("右に列追加");
+    QAction *addLeftColumn = menu.addAction("左に列追加");
+    QAction *deleteColumn = menu.addAction("列削除");
+
+    // メニューを表示し、選ばれたアクションを取得
+    QAction *selectedAction = menu.exec(MainTable_->viewport()->mapToGlobal(pos));
+
+    const int row = index.row();
+    const int column = index.column();
+
+    if (selectedAction == addUpRow)
+    {
+        Unimplemented();
+    }
+    else if (selectedAction == addDownRow)
+    {
+        Unimplemented();
+    }
+    else if (selectedAction == deleteRow)
+    {
+        Unimplemented();
+    }
+    else if (selectedAction == addRightColumn)
+    {
+        Unimplemented();
+    }
+    else if (selectedAction == addLeftColumn)
+    {
+        Unimplemented();
+    }
+    else if (selectedAction == deleteColumn)
+    {
+        Unimplemented();
     }
 }
